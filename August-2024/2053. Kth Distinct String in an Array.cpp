830d8722796bb4200eadb6a6eed56c88e3e2c94b
Ayush Sharma <aayush240204@gmail.com>
2024-08-05 15:01:46 +0530

    Added 2053. Kth Distinct String in an Array.cpp

---
+++ August-2024/2053. Kth Distinct String in an Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mpp;
+
+        for(auto &s : arr){
+            mpp[s]++;
+        }
+
+        int count = 0;
+        for(string &s : arr) {
+            if(mpp[s] == 1) {
+                count++;
+                if(count == k) {
+                    return s;
+                }
+            }
+        }
+
+        return "";
+    }
+};
