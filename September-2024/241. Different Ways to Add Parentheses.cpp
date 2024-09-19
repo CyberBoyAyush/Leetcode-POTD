37bee8f21ce3771229fd3e8dc838f88b4ec949f4
Ayush Sharma <aayush240204@gmail.com>
2024-09-19 20:03:51 +0530

    Update 241. Different Ways to Add Parentheses.cpp

--- September-2024/241. Different Ways to Add Parentheses.cpp
+++ September-2024/241. Different Ways to Add Parentheses.cpp
@@ -1,3 +1,5 @@
+// Appraoch 1 -> Recursion 
+// TC - (n*2^n)
 class Solution {
 public:
     vector<int> diffWaysToCompute(string expression) {
@@ -33,5 +35,52 @@ public:
         } 
 
         return result;
+    }
+};
+
+// Appraoch 2 -> Recursion + Memo
+// TC - O(n^3)
+
+class Solution {
+public:
+    unordered_map<string, vector<int>> memo;    
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int>result;
+
+        // memo call
+        if(memo.find(expression) != memo.end()){
+            return memo[expression];
+        }
+
+        // iterating in expression
+        for(int i = 0 ; i < expression.length(); i++){
+            if(expression[i] == '+' || expression[i] == '-' || expression[i] == '*'){
+                vector<int> left_res = diffWaysToCompute(expression.substr(0,i));
+                vector<int> right_res = diffWaysToCompute(expression.substr(i+1));
+
+                //now we calcualte every possibility
+                for(auto &x:left_res){
+                    for(auto &y:right_res){
+                        if(expression[i] == '+'){
+                            result.push_back(x+y);
+                        }
+                        else if(expression[i] == '*'){
+                            result.push_back(x*y);
+                        }
+                        else{
+                            result.push_back(x-y);
+                        }
+                    }
+                }
+            }
+
+        }
+        
+        // if there is no operator
+        if(result.empty()){
+            result.push_back(stoi(expression));
+        } 
+
+        return memo[expression] = result;
     }
 };
