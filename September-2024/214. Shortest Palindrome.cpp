ee054f0861aeb5fb5cf6d2a052d6e6bc14def581
Ayush Sharma <aayush240204@gmail.com>
2024-09-21 19:14:39 +0530

    Added KMP Appraoch

--- September-2024/214. Shortest Palindrome.cpp
+++ September-2024/214. Shortest Palindrome.cpp
@@ -36,3 +36,49 @@ public:
         return rev + s;
     }
 };
+
+
+// Appraoch 3 -> KMP Algo
+class Solution {
+public:
+    void computeLPS(string pat, vector<int> &LPS, int M){
+        int len = 0;
+        LPS[0] = 0;
+        
+        int i = 1;
+        
+        while(i<M){
+            if(pat[i] == pat[len]){
+                len++;
+                LPS[i] = len;
+                i++;
+            }
+            
+            else{
+                if(len!=0){
+                    len = LPS[len-1];
+                }
+                else{
+                    LPS[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+
+        string temp = s + "-" + rev;
+
+        vector<int> LPS(temp.length(),0);
+
+        computeLPS(temp,LPS,temp.length());
+
+        int longestLPS = LPS[temp.length() - 1];
+
+        string culprit = rev.substr(0,s.length() - longestLPS);
+
+        return culprit + s;
+    }
+};
