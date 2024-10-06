1995c0b86e315ad1c56ed2ae77df6097e49bada0
Ayush Sharma <aayush240204@gmail.com>
2024-10-06 17:17:36 +0530

    Update 2491. Divide Players Into Teams of Equal Skill.cpp

--- October-2024/2491. Divide Players Into Teams of Equal Skill.cpp
+++ October-2024/2491. Divide Players Into Teams of Equal Skill.cpp
@@ -30,3 +30,54 @@ public:
         return chem;
     }
 };
+
+
+
+// Approach 2 -> Counting Sort and Freq with Observations
+// TC - O(n)
+// SC - O(1001)
+
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+       int n = skill.size();
+
+       // count freq vector
+       vector<int> vec(1001,0);
+
+       int SUM = 0;
+
+       for(auto i : skill){
+            SUM += i; // calcualting sum
+            vec[i]++; // updating freq in vec vector
+       } 
+
+       int teams = n / 2; // total number of teams can be there
+       
+       // edge case
+       if(SUM % teams != 0){
+        // teams cant be possible
+        return -1;
+       }
+
+       int s = SUM / teams; // skill which will be of each team
+
+       long long chem = 0;
+
+       for(int i = 0; i<n ; i++){
+        int currSkill = skill[i];
+        int remSkill = s - currSkill;
+
+        if(vec[remSkill] <= 0){
+            return -1; // skill not present cant be possible
+        }
+
+        chem += (long long)(currSkill) * (long long)(remSkill);
+
+        // now decrease remSkill freq
+        vec[remSkill]--;
+       }
+
+       return chem / 2; // bca duplicates will be there
+    }
+};
