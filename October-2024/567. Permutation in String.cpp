c208b66622efbafa71fc32d410513914d757e541
Ayush Sharma <aayush240204@gmail.com>
2024-10-06 19:46:11 +0530

    Update 567. Permutation in String.cpp

--- October-2024/567. Permutation in String.cpp
+++ October-2024/567. Permutation in String.cpp
@@ -1,44 +1,31 @@
+// Approach 1 -> Sorting and Find
+// TC - O(nlogn + m-n)
+// SC - O(1)
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
 
-        // If s1 is larger than s2, no permutation can exist
-        if (n > m) return false;
-
-        // Frequency vectors for s1 and the current window in s2
-        vector<int> s1_freq(26, 0);
-        vector<int> s2_freq(26, 0);
-
-        // Fill frequency of characters in s1
-        for (int i = 0; i < n; i++) {
-            s1_freq[s1[i] - 'a']++;
+        // base edge case
+        if(n > m){
+            return false;
         }
 
-        // Slide the window over s2
-        int i = 0; // left index of the sliding window
-        int j = 0; // right index of the sliding window
-        while (j < m) {
-            // Include a new character from the right end of the window
-            s2_freq[s2[j] - 'a']++;
+        // sort string s1
+        sort(s1.begin(),s1.end());
 
-            // Check if the current window size matches the size of s1
-            if (j - i + 1 > n) {
-                // If we have passed the size of s1, we need to remove the leftmost character
-                s2_freq[s2[i] - 'a']--;
-                i++;
-            }
+        for(int i = 0; i<=m-n ; i++){
+            string substring = s2.substr(i,n);
 
-            // Check if the current window's frequency matches s1's frequency
-            if (s1_freq == s2_freq) {
+            sort(begin(substring),end(substring));
+
+            // now we check
+            if(s1 == substring){
                 return true;
             }
-
-            j++;
         }
 
-        // No matching window found
         return false;
     }
 };
