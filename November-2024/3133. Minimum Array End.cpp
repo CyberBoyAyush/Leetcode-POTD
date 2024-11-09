7f378190c4e6e6ea23243feb8e146deee6693237
Ayush Sharma <aayush240204@gmail.com>
2024-11-10 02:34:57 +0530

    added 3133. Minimum Array End.cpp

---
+++ November-2024/3133. Minimum Array End.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long number = x;
+
+        for(int i = 1; i<n ; i++){
+            number = (number + 1) | x;
+        }
+
+        return number;
+    }
+};
