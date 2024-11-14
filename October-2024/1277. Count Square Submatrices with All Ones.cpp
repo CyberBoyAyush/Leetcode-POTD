ea88dddab08064819ff2ce40e7a6aeaefa0ea68e
Ayush Sharma <aayush240204@gmail.com>
2024-11-15 00:24:00 +0530

    Update 1277. Count Square Submatrices with All Ones.cpp

--- October-2024/1277. Count Square Submatrices with All Ones.cpp
+++ October-2024/1277. Count Square Submatrices with All Ones.cpp
@@ -1,4 +1,13 @@
-// Bottom's UP
+// count-square-submatrices-with-all-ones
+// https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Bottom's Up -> Tabulation using maximal square approach
+// TC : O(n*m)
+// SC : O(n*m)
+
 class Solution {
 public:
     int countSquares(vector<vector<int>>& arr) {
@@ -35,5 +44,85 @@ public:
         }
 
         return sum;
+    }
+};
+
+// Recursive + Memoization
+// TC : O(n*m)
+// SC : O(n*m)
+
+class Solution {
+public:
+    int n, m;
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp){
+        if(i >= n || j >= m) return 0;
+
+        if(grid[i][j] == 0) return 0;
+
+        if(dp[i][j] != -1){
+             return dp[i][j];
+        }
+
+        // right
+        int right = solve(i, j+1, grid, dp);
+        // diagnol
+        int diag = solve(i+1, j+1, grid, dp);
+        // below
+        int below = solve(i+1, j, grid, dp);
+
+        return dp[i][j] = 1 + min({right, diag, below});
+    }
+    int countSquares(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+
+        int result = 0;
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j<m ; j++){
+                result += solve(i,j,grid,dp);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Recursive
+// TC : O(3^n)
+// SC : O(n)
+
+class Solution {
+public:
+    int n, m;
+    int solve(int i, int j, vector<vector<int>>& grid){
+        if(i >= n || j >= m) return 0;
+
+        if(grid[i][j] == 0) return 0;
+
+        // right
+        int right = solve(i, j+1, grid);
+        // diagnol
+        int diag = solve(i+1, j+1, grid);
+        // below
+        int below = solve(i+1, j, grid);
+
+        return 1 + min({right, diag, below});
+    }
+    int countSquares(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+
+        int result = 0;
+
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j<m ; j++){
+                result += solve(i,j,grid);
+            }
+        }
+
+        return result;
     }
 };
