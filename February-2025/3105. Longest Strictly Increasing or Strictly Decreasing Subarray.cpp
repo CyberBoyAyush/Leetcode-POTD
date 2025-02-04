577caee08e29a4a0cf12e22310757fb08b05803d
Ayush Sharma <aayush240204@gmail.com>
2025-02-04 18:42:58 +0530

    Update 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp

--- February-2025/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
+++ February-2025/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
         int n = nums.size();
-        int result = 1;
-        int increasing = 1;
-        int decreasing = 1;
-        //{3, 2, 1}
-        for(int i = 1; i < n; i++) {
-            if(nums[i] > nums[i-1]) {
-                increasing++;
-                decreasing = 1;
-                result = max(result, increasing);
-            } else if(nums[i] < nums[i-1]) {
-                decreasing++;
-                increasing = 1;
-                result = max(result, decreasing);
-            } else {
-                increasing = 1;
-                decreasing = 1;
+
+        int res = 1;
+
+        int inc = 1;
+
+        int dec = 1;
+
+        for(int i=1; i<n ; i++){
+            if(nums[i] > nums[i-1]){
+                // inc
+                inc++;
+                dec = 1;
+                res = max(res, inc);
+            }
+            else if(nums[i]<nums[i-1]){
+                // dec
+                dec++;
+                inc = 1;
+                res = max(res,dec);
+            }
+            else{
+                // same
+                inc = 1;
+                dec = 1;
             }
         }
-        return result;
+
+        return res;
     }
 };
